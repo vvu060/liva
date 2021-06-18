c6c9541a24c6bb5d83baf8b5c94e595d3536e749
ashishmangla1991 <ashishmangla1991@gmail.com>
2021-06-18 20:48:29 +0530

    add reset css

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
+import './App.global.scss';
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-
 import Logo from "./components/logo/Logo";
 import Routes from "./routes";
 function App() {
