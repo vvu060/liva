e3ea55e1f6c9147ae2669ae7d318d669d5f9a874
ashishmangla1991 <ashishmangla1991@gmail.com>
2021-06-19 00:13:50 +0530

    add footer css and footer test

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -2,12 +2,22 @@ import React from "react";
 import style from "./Footer.module.scss";
 
 const Footer = () => {
-  return <footer className={style.footer}>
-    this is footer
-    <div className={style.footer__bottom}>
-      this is copy right
-    </div>
-  </footer>;
+  return (
+    <footer className={style.footer}>
+      <div className={style.footer__top}>
+        <div className="container">
+          <div className="row">
+                  footer top content goes here
+          </div>
+        </div>
+      </div>
+      <div className={style.footer__bottom} data-testid="footerbottom">
+        <div className="container">
+          Copyright &copy; 2021 liva all rights reserved
+        </div>
+      </div>
+    </footer>
+  );
 };
 
 export default Footer;
