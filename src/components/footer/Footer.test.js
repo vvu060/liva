147dc53ccca3fe17044178f5d588a925f511d393
VishalUrankar-2211 <vvu060@gmail.com>
2021-06-20 13:52:20 +0530

    add footer tests

--- src/components/footer/Footer.test.js
+++ src/components/footer/Footer.test.js
@@ -1,12 +1,32 @@
-import{ shallow } from "enzyme";
+import { shallow } from "enzyme";
+import { findByTestAttr } from "../../test/testUtils";
 import Footer from "./Footer";
-test("renders Footer without crashing", () => {
-  const wrapper = shallow(<Footer />);
-  console.log(wrapper.debug());
+/**
+ * Factory function to create a shallowWrapper for Banner component
+ * @function setup
+ * @param {object} props - Component props specific to this setup
+ * @returns {ShallowWrapper}
+ */
+const setup = (props = {}) => {
+  return shallow(<Footer {...props} />);
+};
+
+test("renders footer without crashing", () => {
+  const wrapper = setup();
+  const component = findByTestAttr(wrapper, "component-footer");
+  expect(component.length).toBe(1);
 });
-test("renders Footer bottom", () => {
-    const wrapper = shallow(<Footer />);
-    const footerBottomText = wrapper.find('[data-testid="footerbottom"]').text();
-    console.log(footerBottomText);
+
+describe("renders footer details", () => {
+  const wrapper = setup();
+
+  test("renders company information", () => {
+    const footerInfo = findByTestAttr(wrapper, "footer-info");
+    expect(footerInfo.length).toBe(1);
   });
-  
+
+  test("renders footer links", () => {
+    const footerLinks = findByTestAttr(wrapper, "footer-links");
+    expect(footerLinks.length).toBe(2);
+  });
+});
