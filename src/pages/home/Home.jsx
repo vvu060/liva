e15a2060353148af9c03337d6683e08d00fec308
VishalUrankar-2211 <vvu060@gmail.com>
2021-06-22 12:46:56 +0530

    git error commit

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,13 +1,9 @@
 import React from "react";
 import Footer from "../../components/footer/Footer";
-import "./Home.scss";
+import style from "./Home.module.scss";
 
 const Home = () => {
-  return (
-    <div>
-      I AM HOME
-    </div>
-  );
+  return <div>I AM HOME</div>;
 };
 
 export default Home;
