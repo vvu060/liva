0ae21eabf18e08845875b3f0be55a03a4abe812d
VishalUrankar-2211 <vvu060@gmail.com>
2021-06-22 12:19:21 +0530

    add products row tests

---
+++ src/components/products_row/ProductsRow.test.js
@@ -0,0 +1,10 @@
+import React from "react";
+import { shallow } from "enzyme";
+import ProductRow from "./ProductsRow";
+import { findByTestAttr } from "../../test/testUtils";
+
+test("renders Product Row component without crashing", () => {
+  // const wrapper = shallow(<ProductRow />);
+  // const component = findByTestAttr(wrapper, "component-products-row");
+  // expect(component.length).toBe(1);
+});
