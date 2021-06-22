703f0f9e1064c4baa126cc24227f1fd7ab3fa7da
ashishmangla1991 <ashishmangla1991@gmail.com>
2021-06-22 14:59:44 +0530

    banner commit

--- src/components/banner/Banner.jsx
+++ src/components/banner/Banner.jsx
@@ -1,10 +1,32 @@
-import React from "react";
-
+import React, { useEffect, useState } from "react";
+import style from "./Banner.module.scss";
 const Banner = () => {
+  const [number, setNumber] = useState(0);
+  useEffect(() => setNumber(Math.floor(Math.random() * 2) + 1));
   return (
-    <div data-test="component-banner">
-      <img data-test="component-banner-image" src="" alt="" />
-    </div>
+    <section
+      data-test="component-banner"
+      className={`block ${style.banner}`}
+      role="banner"
+    >
+      <div className={style.banner__wrap}>
+        <div className={style.banner__left}>
+          <h2>
+          -45% Offer All  Products.
+          </h2>
+          <button className="btn btn-primary">
+          shop now
+          </button>
+        </div>
+        <div className={style.banner__right}>
+          <img
+            data-test="component-banner-image"
+            src={`banner-${number}.png`}
+            alt=""
+          />
+        </div>
+      </div>
+    </section>
   );
 };
 
