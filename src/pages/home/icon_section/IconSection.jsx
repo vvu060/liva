f8c68f72f0fc20898d9d1b1d76c574ec037f333f
ashishmangla1991 <ashishmangla1991@gmail.com>
2021-06-26 22:54:40 +0530

    fix css for why us section

--- src/pages/home/icon_section/IconSection.jsx
+++ src/pages/home/icon_section/IconSection.jsx
@@ -7,26 +7,32 @@ import {
   SecurityOutlined,
 } from "@material-ui/icons";
 
-const IconSection = () => {
+const WhyUs = () => {
   return (
     <section className="block">
       <div data-test="component-icon-section" className="container">
-        <div className={`row`}>
-          <div className="col-4">
+        <div className="row">
+          <div className={`col-4 ${style.whyus_item}}`}>
+          <div className={style.whyus_item}>
+                asfsdf dsf s
+            </div>
             <Icon
               Icon={HeadsetMicOutlined}
               title="Online Support"
               desc="We Provide 24/7 Online Support"
             />
           </div>
-          <div className="col-4">
+          <div className={`col-4 ${style.whyus_itemwrap}}`}>
+            <div className={style.whyus_item}>
+                asfsdf dsf s
+            </div>
             <Icon
               Icon={FlightTakeoffOutlined}
               title="Free Shipping"
               desc="Free Home Delivery"
             />
           </div>
-          <div className="col-4">
+          <div className={`col-4 ${style.whyus_item}}`}>
             <Icon
               Icon={SecurityOutlined}
               title="Secure Payment"
@@ -39,4 +45,4 @@ const IconSection = () => {
   );
 };
 
-export default IconSection;
+export default WhyUs;
