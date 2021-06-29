d09399eb529ac2feba3c4282aaa45b56d4801ff3
VishalUrankar-2211 <vvu060@gmail.com>
2021-06-29 15:50:37 +0530

    create cart item shimmer component

---
+++ src/components/loading/cart_item/CartItemShimmer.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import style from "./CartItemShimmer.module.scss";
+
+const CartItemShimmer = ({ colSpace }) => {
+  return (
+    <div
+      data-test="component-shimmer"
+      className={`${style.shimmer} container col-${colSpace}`}
+    >
+      <div className={style.shimmer__wrapper}>
+        <div className={`${style.shimmer__image} ${style.animate}`}></div>
+        <div className={`${style.shimmer__detail} ${style.animate}`}></div>
+        <div className={`${style.shimmer__detail} ${style.animate}`}></div>
+      </div>
+    </div>
+  );
+};
+
+export default CartItemShimmer;
