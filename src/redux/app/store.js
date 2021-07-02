b290eebcff6e24ad9192abba3754b00d41a68cd1
VishalUrankar-2211 <vvu060@gmail.com>
2021-07-02 11:56:09 +0530

    add error slice in redux store

--- src/redux/app/store.js
+++ src/redux/app/store.js
@@ -3,6 +3,7 @@ import userReducer from "../features/user/userSlice";
 import sidebarReducer from "../features/sidebar/sidebarSlice";
 import cartReducer from "../features/cart/cartSlice";
 import loadingReducer from "../features/loading/loadingSlice";
+import errorReducer from "../features/error/errorSlice";
 
 export const store = configureStore({
   reducer: {
@@ -10,5 +11,6 @@ export const store = configureStore({
     sidebar: sidebarReducer,
     cart: cartReducer,
     loading: loadingReducer,
+    error: errorReducer,
   },
 });
