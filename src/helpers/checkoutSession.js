8ce0cadc458aa54626531e13245dff9ca616207b
VishalUrankar-2211 <vvu060@gmail.com>
2021-07-03 07:35:21 +0530

    create helper func to create checkout session

---
+++ src/helpers/checkoutSession.js
@@ -0,0 +1,43 @@
+import { loadStripe } from "@stripe/stripe-js";
+import axios from "axios";
+
+const stripePromise = loadStripe(process.env.REACT_APP_STRIPE_PUBLIC_KEY);
+
+export const checkoutSession = async (items, userEmail) => {
+  const stripe = await stripePromise;
+
+  const headers = new Headers({
+    "Content-Type": "application/json",
+    "Access-Control-Allow-Origin": "*",
+  });
+
+  const transformedItems = items.map((item) => ({
+    price_data: {
+      currency: "INR",
+      product_data: {
+        name: item.product_name,
+      },
+      unit_amount: item.price.raw * 100,
+    },
+    quantity: item.quantity,
+  }));
+
+  const body = {
+    items: transformedItems,
+    email: userEmail,
+  };
+
+  // Call the backend to create a checkout session...
+  const checkoutSession = await axios.post(
+    "http://localhost:5000/create-checkout-session",
+    body,
+    headers
+  );
+
+  // Redirect user/customer to Stripe checkout
+  const result = await stripe.redirectToCheckout({
+    sessionId: checkoutSession.data.id,
+  });
+
+  if (result.error) alert(result.error.message);
+};
