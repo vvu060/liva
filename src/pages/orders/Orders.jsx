9ad5dc13176c7ce7209066343af563756e04ab6d
VishalUrankar-2211 <vvu060@gmail.com>
2021-07-04 10:34:15 +0530

    fetch customer orders

--- src/pages/orders/Orders.jsx
+++ src/pages/orders/Orders.jsx
@@ -1,6 +1,21 @@
 import React from "react";
+import { useSelector } from "react-redux";
+import { endpoints, headersPublic, headersSecret } from "../../endpoints";
+import useFetch from "../../hooks/useFetch";
+import { selectChecUserId } from "../../redux/features/user/userSlice";
 
 const Orders = () => {
+  const userId = useSelector(selectChecUserId);
+  const {
+    response: orders,
+    isError,
+    isLoading,
+  } = useFetch(`${endpoints.customers}/cstmr_yA6nldm9nBwEWb/orders`, {
+    headers: headersSecret,
+  });
+
+  console.log({ orders, userId });
+
   return (
     <div>
       <h1>Orders</h1>
