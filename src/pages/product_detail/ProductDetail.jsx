62a08365f4051c2b97d4ea769ce6410c112a9b53
VishalUrankar-2211 <vvu060@gmail.com>
2021-07-05 16:26:14 +0530

    import prduct detail shimmer for product detail page

--- src/pages/product_detail/ProductDetail.jsx
+++ src/pages/product_detail/ProductDetail.jsx
@@ -4,6 +4,7 @@ import { useDispatch } from "react-redux";
 import Button from "../../components/button/Button";
 import ProductCard from "../../components/products_row/product_card/ProductCard";
 import ProductCardShimmer from "../../components/loading/product_card/ProductCardShimmer";
+import ProductDetailShimmer from "../../components/loading/product_detail/ProductDetailShimmer";
 import { endpoints, headersPublic } from "../../endpoints";
 import { addToCart } from "../../helpers/addToCart";
 import style from "./ProductDetail.module.scss";
@@ -64,83 +65,92 @@ const ProductDetail = (props) => {
 
   return (
     <div className="container block">
-      {details && (
-        <div className={style.product}>
-          <div className={style.product__images}>
-            <div className={style.product__thumb} ref={myRef}>
-              {details.assets.map((image, index) => (
-                <img
-                  id="active"
-                  key={index}
-                  src={image.url}
-                  alt=""
-                  onClick={() => handleActiveTab(index)}
-                />
-              ))}
-            </div>
-            <div className={style.product__bigImage}>
-              <img src={details.assets[index].url} alt="" />
-            </div>
-          </div>
-
-          <div className={style.product__details}>
-            <h2 className={style.product__name}>{details.name}</h2>
-            <p className={style.product__price}>
-              {details.price.formatted_with_symbol}
-            </p>
-            <p className={style.product__desc}>
-              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nesciunt
-              ipsam reiciendis quibusdam? Repellat, suscipit esse sapiente
-              consectetur, recusandae quis optio fugiat alias asperiores saepe
-              sint cupiditate porro fugit. Id, blanditiis!
-            </p>
-            <p className={style.product__unit}>Size: 10g</p>
-            <div className={style.product__qty}>
-              <div className={style.product__qtyInput}>
-                <Remove
-                  className={style.product__icon}
-                  onClick={decreaseQuantity}
-                />
-                <input
-                  type="number"
-                  min={1}
-                  value={quantity}
-                  onChange={(e) => setQuantity(e.target.value)}
-                />
-                <Add
-                  className={style.product__icon}
-                  onClick={increaseQuantity}
-                />
+      {isLoading ? (
+        <Fragment>
+          <ProductDetailShimmer />
+        </Fragment>
+      ) : (
+        <Fragment>
+          {details && (
+            <div className={style.product}>
+              <div className={style.product__images}>
+                <div className={style.product__thumb} ref={myRef}>
+                  {details.assets.map((image, index) => (
+                    <img
+                      id=""
+                      key={index}
+                      src={image.url}
+                      alt={index}
+                      onClick={() => handleActiveTab(index)}
+                    />
+                  ))}
+                </div>
+                <div className={style.product__bigImage}>
+                  <img src={details.assets[index].url} alt="" />
+                </div>
               </div>
-              <Button
-                classes="btn btn-primary"
-                name="Add to Cart"
-                onClick={addItemToCart}
-              />
+
+              <div className={style.product__details}>
+                <h2 className={style.product__name}>{details.name}</h2>
+                <p className={style.product__price}>
+                  {details.price.formatted_with_symbol}
+                </p>
+                <p className={style.product__desc}>
+                  Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                  Nesciunt ipsam reiciendis quibusdam? Repellat, suscipit esse
+                  sapiente consectetur, recusandae quis optio fugiat alias
+                  asperiores saepe sint cupiditate porro fugit. Id, blanditiis!
+                </p>
+                <p className={style.product__unit}>Size: 10g</p>
+                <div className={style.product__qty}>
+                  <div className={style.product__qtyInput}>
+                    <Remove
+                      className={style.product__icon}
+                      onClick={decreaseQuantity}
+                    />
+                    <input
+                      type="number"
+                      min={1}
+                      value={quantity}
+                      onChange={(e) => setQuantity(e.target.value)}
+                    />
+                    <Add
+                      className={style.product__icon}
+                      onClick={increaseQuantity}
+                    />
+                  </div>
+                  <Button
+                    classes="btn btn-primary"
+                    name="Add to Cart"
+                    onClick={addItemToCart}
+                  />
+                </div>
+                <div className={style.product__share}>
+                  <a href="https://www.facebook.com/" target="_blank">
+                    <img
+                      src="https://cdn2.iconfinder.com/data/icons/social-media-2285/512/1_Facebook_colored_svg_copy-256.png"
+                      alt=""
+                    />
+                  </a>
+                  <a href="https://www.whatsapp.com/" target="_blank">
+                    <img
+                      src="https://cdn4.iconfinder.com/data/icons/social-media-logos-6/512/13-whatsapp-256.png"
+                      alt=""
+                    />
+                  </a>
+                  <a href="https://www.google.com/" target="_blank">
+                    <img
+                      src="https://cdn3.iconfinder.com/data/icons/logos-brands-3/24/logo_brand_brands_logos_google-256.png"
+                      alt=""
+                    />
+                  </a>
+                </div>
+              </div>
             </div>
-            <div className={style.product__share}>
-              <a href="https://www.facebook.com/" target="_blank">
-                <img
-                  src="https://cdn2.iconfinder.com/data/icons/social-media-2285/512/1_Facebook_colored_svg_copy-256.png"
-                  alt=""
-                />
-              </a>
-              <a href="https://www.whatsapp.com/" target="_blank">
-                <img
-                  src="https://cdn4.iconfinder.com/data/icons/social-media-logos-6/512/13-whatsapp-256.png"
-                  alt=""
-                />
-              </a>
-              <a href="https://www.google.com/" target="_blank">
-                <img
-                  src="https://cdn3.iconfinder.com/data/icons/logos-brands-3/24/logo_brand_brands_logos_google-256.png"
-                  alt=""
-                />
-              </a>
-            </div>
-          </div>
-        </div>
+          )}
+        </Fragment>
       )}
+
       <div class={`block ${style.product__related}`}>
         <h3>Related Products</h3>
         <div className={style.product__relatedProduct}>
