2778f4c7cca5d909c6e5a95993f42045b03a0d09
VishalUrankar-2211 <vvu060@gmail.com>
2021-07-09 14:33:37 +0530

    add tests for order component

--- src/pages/orders/order/Order.test.js
+++ src/pages/orders/order/Order.test.js
@@ -113,9 +113,191 @@ const setup = (props = {}) => {
 };
 
 test("renders order component without error", () => {
-  const wrapper = setup({ defaultProps });
+  const wrapper = setup({ order: defaultProps });
   const component = findByTestAttr(wrapper, "component-order");
   expect(component.length).toBe(1);
+});
+
+describe("renders order created date", () => {
+  test("does not render created date when `order` prop is not available", () => {
+    const wrapper = setup({ order: {} });
+    const createdDate = findByTestAttr(wrapper, "created-date");
+    expect(createdDate.text()).toBe("");
+  });
+
+  test("renders created date when `order` prop is available", () => {
+    const wrapper = setup({ order: defaultProps });
+    const createdDate = findByTestAttr(wrapper, "created-date");
+    expect(createdDate.text().length).not.toBe(0);
+  });
+});
+
+describe("renders order total amount", () => {
+  test("does not render order total amount when `order` prop is not available", () => {
+    const wrapper = setup({ order: {} });
+    const orderTotal = findByTestAttr(wrapper, "order-total");
+    expect(orderTotal.text()).toBe("");
+  });
+
+  test("renders order total amount when `order` prop is available", () => {
+    const wrapper = setup({ order: defaultProps });
+    const orderTotal = findByTestAttr(wrapper, "order-total");
+    expect(orderTotal.text().length).not.toBe(0);
+  });
+});
+
+describe("renders order ID", () => {
+  test("does not render order ID when `order` prop is not available", () => {
+    const wrapper = setup({ order: {} });
+    const orderId = findByTestAttr(wrapper, "order-id");
+    expect(orderId.text()).toBe("");
+  });
+
+  test("renders order ID when `order` prop is available", () => {
+    const wrapper = setup({ order: defaultProps });
+    const orderId = findByTestAttr(wrapper, "order-id");
+    expect(orderId.text().length).not.toBe(0);
+  });
+});
+
+describe("renders product name", () => {
+  test("does not render product name when `order` props is not available", () => {
+    const wrapper = setup({ order: {} });
+    const name = findByTestAttr(wrapper, "product-name");
+    expect(name.text()).toBe("");
+  });
+
+  test("renders product name when `order` props is available", () => {
+    const wrapper = setup({ order: defaultProps });
+    const name = findByTestAttr(wrapper, "product-name");
+    expect(name.text().length).not.toBe(0);
+  });
+});
+
+describe("renders product price", () => {
+  test("does not render product price when `order` props is not available", () => {
+    const wrapper = setup({ order: {} });
+    const price = findByTestAttr(wrapper, "product-price");
+    expect(price.text()).toBe("");
+  });
+
+  test("renders product price when `order` props is available", () => {
+    const wrapper = setup({ order: defaultProps });
+    const price = findByTestAttr(wrapper, "product-price");
+    expect(price.text().length).not.toBe(0);
+  });
+});
+
+describe("renders product quantity", () => {
+  test("does not render product quantity when `order` props is not available", () => {
+    const wrapper = setup();
+    const quantity = findByTestAttr(wrapper, "product-quantity");
+    expect(quantity.text()).toBe("");
+  });
+
+  test("renders product quantity when `order` props is available", () => {
+    const wrapper = setup({ order: defaultProps });
+    const quantity = findByTestAttr(wrapper, "product-quantity");
+    expect(quantity.text().length).not.toBe(0);
+  });
+});
+
+describe("renders product total", () => {
+  test("does not render product total when `order` props is not available", () => {
+    const wrapper = setup({ order: {} });
+    const total = findByTestAttr(wrapper, "product-total");
+    expect(total.text()).toBe("");
+  });
+
+  test("renders product total when `order` props is available", () => {
+    const wrapper = setup({ order: defaultProps });
+    const total = findByTestAttr(wrapper, "product-total");
+    expect(total.text().length).not.toBe(0);
+  });
+});
+
+describe("renders shipping address name", () => {
+  test("does not render shipping address name when `order` props is not available", () => {
+    const wrapper = setup({ order: {} });
+    const shippingName = findByTestAttr(wrapper, "shipping-name");
+    expect(shippingName.text()).toBe("");
+  });
+
+  test("renders shipping address name when `order` props is available", () => {
+    const wrapper = setup({ order: defaultProps });
+    const shippingName = findByTestAttr(wrapper, "shipping-name");
+    expect(shippingName.text().length).not.toBe(0);
+  });
+});
+
+describe("renders shipping address street", () => {
+  test("does not render shipping address street when `order` props is not available", () => {
+    const wrapper = setup({ order: {} });
+    const shippingStreet = findByTestAttr(wrapper, "shipping-street");
+    expect(shippingStreet.text()).toBe("");
+  });
+
+  test("renders shipping address street when `order` props is available", () => {
+    const wrapper = setup({ order: defaultProps });
+    const shippingStreet = findByTestAttr(wrapper, "shipping-street");
+    expect(shippingStreet.text().length).not.toBe(0);
+  });
+});
+
+describe("renders shipping address street2", () => {
+  test("does not render shipping address street2 when `order` props is not available", () => {
+    const wrapper = setup({ order: {} });
+    const shippingStreet2 = findByTestAttr(wrapper, "shipping-street2");
+    expect(shippingStreet2.text()).toBe("");
+  });
+
+  test("renders shipping address street2 when `order` props is available", () => {
+    const wrapper = setup({ order: defaultProps });
+    const shippingStreet2 = findByTestAttr(wrapper, "shipping-street2");
+    expect(shippingStreet2.text().length).not.toBe(0);
+  });
+});
+
+describe("renders shipping address city", () => {
+  test("does not render shipping address city when `order` props is not available", () => {
+    const wrapper = setup({ order: {} });
+    const shippingCity = findByTestAttr(wrapper, "shipping-city");
+    expect(shippingCity.text()).toBe("");
+  });
+
+  test("renders shipping address city when `order` props is available", () => {
+    const wrapper = setup({ order: defaultProps });
+    const shippingCity = findByTestAttr(wrapper, "shipping-city");
+    expect(shippingCity.text().length).not.toBe(0);
+  });
+});
+
+describe("renders shipping address pincode", () => {
+  test("does not render shipping address pincode when `order` props is not available", () => {
+    const wrapper = setup({ order: {} });
+    const shippingPincode = findByTestAttr(wrapper, "shipping-pincode");
+    expect(shippingPincode.text()).toBe("");
+  });
+
+  test("renders shipping address pincode when `order` props is available", () => {
+    const wrapper = setup({ order: defaultProps });
+    const shippingPincode = findByTestAttr(wrapper, "shipping-pincode");
+    expect(shippingPincode.text().length).not.toBe(0);
+  });
+});
+
+describe("renders shipping address state", () => {
+  test("does not render shipping address state when `order` props is not available", () => {
+    const wrapper = setup({ order: {} });
+    const shippingState = findByTestAttr(wrapper, "shipping-state");
+    expect(shippingState.text()).toBe("");
+  });
+
+  test("renders shipping address state when `order` props is available", () => {
+    const wrapper = setup({ order: defaultProps });
+    const shippingState = findByTestAttr(wrapper, "shipping-state");
+    expect(shippingState.text().length).not.toBe(null);
+  });
 });
 
 test("does not throw warning with expected props", () => {
